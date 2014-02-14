ff0adaf21e9f966dd81098607ac6f773bdee9d03
ph3b <mathiasiden@me.com>
2014-02-14 17:23:37 +0100

    Commit alpha

--- app/scripts/controllers/ferdig.js
+++ app/scripts/controllers/ferdig.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('shoppinglistApp')
-  .controller('FerdigCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('FerdigCtrl', function ($scope, $firebase, $route) {
+        $scope.arkivList = $firebase(new Firebase("https://xlib.firebaseio.com/slapp/arkiv"));
+        $scope.arkivList.$on("change", function () {
+            $scope.getLength = $scope.arkivList.$getIndex().length
+        });
+        $scope.emptyArchive = function () {
+            $scope.arkivList.$remove();
+        }
+    });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,7 +3,9 @@
 angular.module('shoppinglistApp')
     .controller('MainCtrl', function ($scope, $firebase) {
         //Lager todolist-objektet
-        $scope.todoList = $firebase(new Firebase("https://xlib.firebaseio.com/todo"));
+        $scope.todoList = $firebase(new Firebase("https://xlib.firebaseio.com/slapp/todo"));
+        $scope.arkivList = $firebase(new Firebase("https://xlib.firebaseio.com/slapp/arkiv"));
+
         //Noen ekle globale variabler
         //TODO: Kan disse variablene fjernes?
         $scope.placeHolder = "Ny matvare";
@@ -45,13 +47,14 @@ angular.module('shoppinglistApp')
         }
         //Lagre mat etter redigert
         $scope.saveFood = function () {
-            $scope.itemPath = $firebase(new Firebase("https://xlib.firebaseio.com/todo/" + $scope.currentId));
+            $scope.itemPath = $firebase(new Firebase("https://xlib.firebaseio.com/slapp/todo/" + $scope.currentId));
             if ($scope.item != undefined && $scope.item != "") {
                 $scope.itemPath.matvare = $scope.item;
                 $scope.itemPath.$save("matvare");
                 $scope.currentId = "";
                 $scope.item = "";
                 $scope.showSave = false;
+                $scope.placeHolder = "Ny matvare";
             }
             else {
                 $scope.item = $scope.todoList[$scope.currentId].matvare
@@ -64,7 +67,7 @@ angular.module('shoppinglistApp')
         }
         //Kryss av kjøpte produkter
         $scope.checkItem = function (id) {
-            $scope.itemPath = $firebase(new Firebase("https://xlib.firebaseio.com/todo/" + id));
+            $scope.itemPath = $firebase(new Firebase("https://xlib.firebaseio.com/slapp/todo/" + id));
             if ($scope.todoList[id].isChecked == false) {
                 $scope.itemPath.isChecked = true;
             }
@@ -73,27 +76,40 @@ angular.module('shoppinglistApp')
             }
             $scope.itemPath.$save("isChecked");
         }
-       /* Arkiv-funksjon som flytter todo til todo/archive
-           $scope.addToArchive = function(){
-            $scope.itemPath = $firebase(new Firebase("https://xlib.firebaseio.com/todo/" + id));
-            if ($scope.todoList[id].archived == false) {
-                $scope.itemPath.archived = true;
-            }
-            $scope.itemPath.$save("archived");
-        }*/
+        $scope.addToArchive = function () {
+            var keys = $scope.todoList.$getIndex();
+            keys.forEach(function (key, i) {
+                if ($scope.todoList[key].isChecked == true) {
+                    $scope.arkivList.$add($scope.todoList[key]);
+                    $scope.todoList.$remove(key);
+                }
+            })
+        }
         //Lytter på write-to backend og setter id-attr = parent index for å lettere hente ut entries
         $scope.todoList.$on('loaded', function () { //Genialt!!!!
             $scope.todoList.$on('child_added', function (val) {
-                $scope.itemPath = $firebase(new Firebase("https://xlib.firebaseio.com/todo/" + val.snapshot.name));
+                $scope.itemPath = $firebase(new Firebase("https://xlib.firebaseio.com/slapp/todo/" + val.snapshot.name));
                 $scope.itemPath.id = val.snapshot.name;
                 if (val.snapshot.value.matvare != undefined && val.snapshot.value.matvare != "") {//Må bruke snapshot value her.
                     $scope.itemPath.$save("id");
                 }
             });
         });
-        //Lytter på todoList-objektet og holde getLength oppdatert
+        //lytter på arkivlist
+        $scope.arkivList.$on('loaded', function () { //Genialt!!!!
+            $scope.arkivList.$on('child_added', function (val) {
+                $scope.arkivPath = $firebase(new Firebase("https://xlib.firebaseio.com/slapp/arkiv/" + val.snapshot.name));
+                $scope.arkivPath.id = val.snapshot.name;
+                if (val.snapshot.value.matvare != undefined && val.snapshot.value.matvare != "") {//Må bruke snapshot value her.
+                    $scope.arkivPath.$save("id");
+                }
+            });
+        });
+        //Lytter på todoList-objektet og holder getLength oppdatert
         $scope.todoList.$on("change", function () {
             $scope.getLength = $scope.todoList.$getIndex().length
         });
+        $scope.arkivList.$on("change", function () {
+            $scope.getLengthArkiv = $scope.arkivList.$getIndex().length
+        });
     });
-//Til Rebekka <3
